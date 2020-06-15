ac85feca7a6e94a2bb6a0a6999221ae0e172131d
정동현 <stjhyeon@gmail.com>
2020-06-15 18:24:06 +0900

    source git

--- app.py
+++ app.py
@@ -7,26 +7,27 @@ from flask import Flask,request,render_template,url_for,redirect
 app = Flask(__name__)
 
 def check_url(url):
-    regex = re.compile(
-        r'^https?://'
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'
-        r'localhost|'
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-        r'(?::\d+)?'
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    if(url is not None):
-        return regex.search(url).group(0) 
-
-@app.route('/error', methods=["GET"])
-def error_page():
-    return render_template('error.html')
+    try:
+        regex = re.compile(
+            r'^https?://'
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'
+            r'localhost|'
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+            r'(?::\d+)?'
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        if(url is not None):
+            return regex.search(url).group(0)
+        elif(url is None):
+            return '''please input'''
+    except:
+        return '''error occured'''
 
 @app.route('/', methods=["GET","POST"])
 def index():
     # data save
     if request.method == 'POST':
         url = request.form.get('url')
-        url = check_url(url)
+        check_url(url)
         use_cookie = request.form.get('use_cookie')
     
         if(use_cookie is not None):
