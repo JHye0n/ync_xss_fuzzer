72b5d2f85fbc5b260558225cb60786e17c235b07
정동현 <stjhyeon@gmail.com>
2020-06-24 14:07:06 +0900

    login,register update - jhyeon

--- app.py
+++ app.py
@@ -2,29 +2,36 @@
 #!/usr/bin/python3
 
 from core import *
-import urllib.parse
+from urllib.parse import urlparse
 from urllib.error import URLError, HTTPError
-from flask import Flask,request,render_template,url_for,redirect
+from flask import Flask,request,render_template,url_for,redirect,g
+from passlib.hash import sha256_crypt
 import re
+import sqlite3
 
 app = Flask(__name__)
-
-def check_url(url):
-    try:
-        regex = re.compile(
-            r'^https?://'
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-            r'(?::\d+)?'
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-        if(url is not None):
-            return regex.search(url).group(0)
-    except:
-        None
+DATABASE = 'users.db'
 
 def vaild_url(url):
-    url_scheme = urllib.parse.urlparse(url).scheme
-    url_domain = urllib.parse.urlparse(url).hostname
+    url_scheme = urlparse(url).scheme
+    url_domain = urlparse(url).netloc
+
+    if(url_scheme != 'http','https'):
+        return render_template('/error/url_error.html')
+
+    if(url_domain == 'localhost' or '127.0.0.1' or ' '):
+        return render_template('/error/url_error.html')
+
+def load_db():
+    if not hasattr(g, 'users.db'):
+        db = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    if hasattr(g, 'users.db'):
+      g.db.close()
+
 
 @app.route('/', methods=["GET","POST"])
 def index():
@@ -32,26 +39,51 @@ def index():
 
 @app.route('/login', methods=["GET"])
 def login_page():
-    return '''login page'''
+    return render_template('login.html')
 
 @app.route('/register', methods=["GET"])
 def register_page():
-    return '''register page'''
+    return render_template('register.html')
 
 @app.route('/api/login', methods=["POST"])
 def login():
-    return '''login go'''
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    db = load_db()
+    cursor = db.cursor()
+    query = cursor.execute("SELECT * FROM users where username = ? AND password = ?", (username, sha256_crypt.encrypt(password))).fetchone()
+
+    if query:
+        return '''goood'''
+    else:
+        return '<script>alert("login failed"); history.go(-1); </script>'
 
 @app.route('/api/register', methods=["POST"])
 def register():
-    return '''register go'''
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    db = load_db()
+    cursor = db.cursor()
+    query = cursor.execute("SELECT * FROM users where username = ?", (username)).fetchone()
+
+    if query:
+        return '<script>alert("already exist account"); history.go(-1); </script>'
+    else:
+        query = "INSERT INTO users(username, password) VALUES (?,?)"
+        query_exec = cursor.execute(query, (username, sha256_crypt.encrypt(password)))
+        db.commit()
+        return '''registeer ok'''
 
 @app.route('/api/search', methods=["POST"])
 def search():
-    try:
-        url = request.form.get('url')
-        use_cookie = request.form.get('use_cookie')
+    url = request.form.get('url')
+    use_cookie = request.form.get('use_cookie')
 
+    vaild_url(url)
+
+    if(vaild_url is not None):
         if(use_cookie is not None):
             cookie = request.form.get('cookie')
             if cookie and not cookie.isspace():
@@ -66,14 +98,7 @@ def search():
             parser.run()
             result = parser.result
             return render_template('index.html', result=result)
-
-    except HTTPError as e:
-        return render_template('error/url_error.html')
-
-    except ValueError as e:
-        return render_template('error/url_error.html')
-
-    except URLError as e:
-        return render_template('error/url_error.html')
+    else:
+        print('vaild url error')
 
 app.run('0.0.0.0',8000)
