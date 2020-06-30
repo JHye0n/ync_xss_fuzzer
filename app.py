daca3e70076c4fdcf3ca2822a9ae736f414399a6
정동현 <stjhyeon@gmail.com>
2020-06-30 18:55:10 +0900

    finally update(1)

--- app.py
+++ app.py
@@ -1,128 +1,202 @@
-#dev by, jhyeon(stjhyeon@kakao.com) & nga & seonghyun park
-#!/usr/bin/python3
-
 from core import *
 from urllib.parse import urlparse
 from urllib.error import URLError, HTTPError
 from flask import Flask,request,render_template,url_for,redirect,g,session,url_for
-import re
 import sqlite3
 import hashlib
-import os
 import json
+import os
 
 app = Flask(__name__, static_url_path="/static/", static_folder="./templates/static/")
-DATABASE = 'users.db'
+USER_DATABASE = 'users.db'
+REPORT_DATABASE = 'report.db'
 app.secret_key = os.urandom(24)
 
 def vaild_url(url):
 
-    url_scheme = urlparse(url).scheme
-    url_domain = urlparse(url).netloc
+	url_scheme = urlparse(url).scheme
+	url_domain = urlparse(url).netloc
 
-    if(url_scheme == ''):
-        return None
+	if(url_scheme == ''):
+	
+		return False
 
-    if(url_domain == ''):
-        return None
+	if(url_domain == ''):
+	
+		return False
+	
+	if(url_scheme == 'http' or url_scheme == 'https'):
 
-    if(url_scheme == 'http' or url_scheme == 'https'):
-        if(url_domain == 'localhost' or url_domain == '127.0.0.1'):
-            return None
-        else:
-            return url
+		if(url_domain == 'localhost' or url_domain == '127.0.0.1'):
 
-def load_db():
-    if not hasattr(g, 'users.db'):
-        db = sqlite3.connect(DATABASE)
-    db.row_factory = sqlite3.Row
-    return db
+			return False
+		else:
+			return url
+
+def load_userdb():
+
+	if not hasattr(g, 'users.db'):
+	
+		db = sqlite3.connect(USER_DATABASE)
+	
+	db.row_factory = sqlite3.Row
+	return db
+
+def load_reportdb():
+
+	if not hasattr(g, 'report.db'):
+
+		db = sqlite3.connect(REPORT_DATABASE)
+
+	db.row_factory = sqlite3.Row
+	return db
 
 @app.teardown_appcontext
 def close_connection(exception):
-    if hasattr(g, 'users.db'):
-      g.db.close()
+
+	if hasattr(g, 'users.db'):
+
+		g.db.close()
 
 
 @app.route('/', methods=["GET","POST"])
 def index():
+
         return render_template('index.html')
 
-@app.route('/login', methods=["GET"])
-def login_page():
-    if session.get('user') is not None:
-        return render_template('main.html')
-    else:
-        return render_template('login.html')
-
-@app.route('/logout', methods=["GET"])
+@app.route('/api/logout', methods=["GET"])
 def logout():
-    session.pop('user', None)
-    return redirect(url_for('index'))
 
-@app.route('/register', methods=["GET"])
-def register_page():
-    if session.get('user') is not None:
-        return render_template('main.html')
-    else:
-        return render_template('register.html')
+	if session.get('user') == None:
+
+		return json.dumps({'success':"false", "msg":"You are not logged"})
+
+	session.pop('user', None)
+	return json.dumps({"success":"true", "msg":"Logout Success"})
+
 
 @app.route('/api/login', methods=["POST"])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
 
-    db = load_db()
-    cursor = db.cursor()
-    query = cursor.execute("SELECT * FROM users where username = ? AND password = ?", (username, hashlib.sha256(password.encode()).hexdigest())).fetchone()
+	if session.get('user') != None:
 
-    if query:
-        session['user'] = query['username']
-        return render_template('main.html')
-    else:
-        return '<script>alert("login failed"); history.go(-1); </script>'
+		return json.dumps({'success':"false","msg":"You are already logged"})
+
+	username = request.form.get('username')
+	password = request.form.get('password')
+
+	db = load_userdb()
+	cursor = db.cursor()
+	query = cursor.execute("SELECT username FROM users where username = ? AND password = ?", (username, hashlib.sha256(password.encode()).hexdigest())).fetchone()
+
+	if query:
+	
+		session['user'] = query['username']
+		return json.dumps({"success":"true","msg":"Login Success"});
+
+	else:
+
+		return json.dumps({"success":"false","msg":"Login Fail"});
+
 
 @app.route('/api/register', methods=["POST"])
 def register():
-    username = request.form.get('username')
-    password = request.form.get('password')
 
-    db = load_db()
-    cursor = db.cursor()
-    query = cursor.execute("SELECT * FROM users where username = ?", (username,)).fetchone()
+	if session.get('user') != None:
 
-    if query:
-        return '<script>alert("already exist account"); history.go(-1); </script>'
+		return json.dumps({'success':"false","msg":"You are already logged"})
 
-    query2 = "INSERT INTO users(username, password) VALUES (?,?)"
-    query_exec = cursor.execute(query2, (username, hashlib.sha256(password.encode()).hexdigest()))
-    db.commit()
-    return redirect(url_for('login_page'))
+	username = request.form.get('username')
+	password = request.form.get('password')
 
-    if not query_exec:
-        return '<script>alert("error occured"); history.go(-1); </script>'
+	db = load_userdb()
+	cursor = db.cursor()
+	query = cursor.execute("SELECT * FROM users where username = ?", (username,)).fetchone()
+
+	if query:
+
+		return json.dumps({"success":"false", "msg": "User already exist"});
+
+
+	query2 = "INSERT INTO users(username, password) VALUES (?,?)"
+	query_exec = cursor.execute(query2, (username, hashlib.sha256(password.encode()).hexdigest()))
+	db.commit()
+
+	if not query_exec:
+	
+		return json.dumps({"success":"false","msg":"DB Error"});
+	
+	return json.dumps({"success":"true", "msg":"Register Success"});
 
 @app.route('/api/search', methods=["POST"])
 def search():
-    if session.get('user') is None:
-        return json.dumps({"login":"false"})
-    url = request.form.get('url')
-    use_cookie = request.form.get('use_cookie')
+    
+	if session.get('user') == None:
 
-    check_url = vaild_url(url)
-    if(check_url is not None):
-        if(use_cookie == 'true'):
-            cookie = request.form.get('cookie')
-            parser = XSsearch(url=url,cookies=cookie)
-            parser.run()
-            result = parser.result
-            return json.dumps(result)
-        else:
-            parser = XSsearch(url=url,cookies={})
-            parser.run()
-            result = parser.result
-            return json.dumps(result)
-    else:
-            return json.dumps({"success":"false"})
+		return json.dumps({'success':"false","msg":"You are not logged"})
+	
+	url = request.form.get('url')
+	use_cookie = request.form.get('use_cookie')
 
-app.run('127.0.0.1',8000)
+	check_url = vaild_url(url)
+
+	if(check_url == False):
+
+		return json.dumps({"success":"false", "msg": "Invaild URL"})
+
+	if(use_cookie == 'true'):
+
+		cookie = request.form.get('cookie')
+		parser = XSsearch(url=url,cookies=cookie)
+		parser.run()
+		result = parser.result
+		return json.dumps(result)
+
+	else:
+
+		parser = XSsearch(url=url,cookies={})
+		parser.run()
+		result = parser.result
+		return json.dumps(result)
+
+
+@app.route('/api/status', methods=["GET"])
+def status():
+
+	logined = True if session.get('user') else False
+	return json.dumps({"success":logined, "msg":"Logged"})
+	
+
+@app.route('/api/report', methods=["POST"])
+def report():
+
+	if session.get('user') == None:
+   
+		return json.dumps({'success':"false","msg":"You arg not logged"})
+	
+	url = request.form.get('url')	
+
+	if(vaild_url(url) == False):
+	
+		return json.dumps({"success":"false","msg":"Invaild URL"})
+	
+	
+	username = session.get('user')
+	db = load_reportdb() 
+	cursor = db.cursor()
+	
+	query = "INSERT INTO report(username, url) VALUES (?,?)"
+	query_exec = cursor.execute(query, (username, url))
+	db.commit()
+
+	if not query_exec:
+
+		return json.dumps({"success":"false","msg":"DB Error"})
+	
+	return json.dumps({"success":"true", "msg":"Report Success"})
+
+
+#crt ="/etc/letsencrypt/live/test.nga.observer-0001/cert.pem"
+#key = "/etc/letsencrypt/live/test.nga.observer-0001/privkey.pem"
+#app.run('0.0.0.0',443,ssl_context=(crt,key))
+app.run('0.0.0.0',8000)
--- templates/static/js/index.js
+++ templates/static/js/index.js
@@ -3,16 +3,23 @@ function search(){
     var url = $("#url").val();
     var chk = $("#chk").is(":checked");
     var cookie = $("#cookie").val();
-    console.log(url);
     $.ajax({
 
         type:"POST"
-        ,url: "http://127.0.0.1:8000/api/search"
-        ,data :{ URL_DATA: url, use_cookie: chk, cookie: cookie }
+        ,url: "/api/search"
+        ,data :{ url: url, use_cookie: chk, cookie: cookie }
         ,success: function(res){
 
-            $('#Result_modal').modal('show')
-            console.log(res)
+	    res = JSON.parse(res)
+	    console.log(res)
+	    if(res.success == 'false'){
+
+		return alert(res.msg);
+
+	    }
+
+            get_vuln(res)
+	    $('#Result_modal').modal('show');
 
         }
         ,beforeSend:function(){
@@ -32,6 +39,178 @@ function search(){
         }
     });
 }
+
+function login(){ 
+
+	var username = $("#Lusername").val(); 
+	var password = $("#Lpassword").val();
+
+	$.ajax({
+
+     		type:"POST"
+        	,url: "/api/login"
+        	,data :{ username: username, password: password}
+        	,success: function(res){
+
+			res = JSON.parse(res)
+			console.log(res)
+			if(res.success == 'true'){
+
+				alert(res.msg);
+				location.reload();
+			
+			}
+			else{
+
+				alert(res.msg);		
+
+			}
+	
+	        }
+
+	});
+}
+function Register(){
+
+        var username = $("#Rusername").val();
+        var password = $("#Rpassword").val();
+
+        $.ajax({
+
+                type:"POST"
+                ,url: "/api/register"
+                ,data :{ username: username, password: password}
+                ,success: function(res){
+
+			
+			res = JSON.parse(res)
+			console.log(res)
+                        if(res.success == 'true'){
+
+                                alert(res.msg);
+				$('#Register_modal').modal('hide');$('#Login_modal').modal('show');
+				return true;
+
+                        }
+                        if(res.success == 'false'){
+
+                                alert(res.msg);
+				return false;		
+
+                        }
+
+                }
+
+        });
+}
+
+function get_vuln(xs){
+
+	p = new RegExp("_XS_.*?['\"<>]+.*?_XS_")
+	div = "<div><br>"
+	
+	xs.forEach(function(element){
+		
+		for(var key in element){
+
+			if(key != 'line'){
+				console.log(p.exec(element[key]))
+				if(p.exec(element[key])){
+				
+					find = p.exec(element[key])[0].replace(/\</gi,"&lt;").replace(/\>/,"&gt;");
+					data = element[key].replace(/\</gi,"&lt;");
+					data = data.replace(/\>/gi,"&gt;");
+					data = (element['line']+1)+" | " +data.replace(find,"<span class='vuln' style='color: red;'>"+find.replace(/&/g, "&amp;")+"</span>");
+					data = '<div>Parameter: '+key+' </div><div>Type: Vulnerablity!</div><pre class="line-numbers"><code class="language-markup">'+data+' </code> </pre>';
+					div += data
+				}
+
+			}
+
+		}
+
+	});
+	div += "<br></div>"
+	console.log(div)
+
+	if(div == "<div><br><br></div>")
+		div = "<div><br><div style='text-align:center;'>Not Found!</div><br></div";
+
+	$('.Result_modal-body').html(div)
+
+
+}
+function GetLoginInfo(){
+
+	$.ajax({
+ 	
+		type:"GET"
+                ,url: "/api/status"
+                ,success: function(res){
+
+			res = JSON.parse(res)
+			console.log(res)
+
+			if(res.success == true){
+
+				$('#login_out').html("<i class='fas fa-sign-out-alt'></i> Logout");
+				$('#login_out').attr("onclick","logout()");
+				return true;				
+
+			}
+		}
+
+	});
+
+}
+function logout(){
+
+	$.ajax({
+
+		type:"GET",
+		url: "/api/logout",
+		success: function(res){
+
+			res = JSON.parse(res);
+			alert(res.msg);
+			location.reload();
+
+		}
+	});
+	
+
+}
+
+function Report(){
+
+        var url = $("#Rurl").val();
+
+        $.ajax({
+
+                type:"POST"
+                ,url: "/api/report"
+                ,data :{ url:url}
+                ,success: function(res){
+
+                        res = JSON.parse(res)
+			if(res.success == "true"){
+
+				alert(res.msg);
+				location.reload();
+				return true;
+			}
+			else{
+
+				alert(res.msg);
+				return false;
+
+			}
+                        console.log(res)
+                }
+
+        });
+}
+
 function cookie_box(){
 
     var chk = document.getElementById('chk');
@@ -46,6 +225,6 @@ function cookie_box(){
         document.getElementById('cookie').setAttribute('class', 'cookieTerm display-none');
   
    }
-
 }
 
+GetLoginInfo();
